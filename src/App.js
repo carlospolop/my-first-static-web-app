608d9aa5921a1c381b24c1495de2533bf3cffab7
rhalyc <32094066+rhalyc@users.noreply.github.com>
2025-01-06 13:16:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value} from rhalyc <script>alert(1)</script></div>;
+  return <div>Hello {value} from rhalyc2 <script>alert(1)</script></div>;
 }
 
 export default App;
